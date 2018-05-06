64e3b53def0a67291f5e67a5556fabf910cf0816
Yang Ke <saint_yk@163.com>
2018-05-06 09:37:42 +0800

    Fix eslint errors

--- 4-2/src/components/a.js
+++ 4-2/src/components/a.js
@@ -1,22 +1,21 @@
-export function renderA() {
-  var a = document.getElementById('one');
+export function renderA () {
+  var a = document.getElementById('one')
   a.innerHTML = `
     <ul>
       <li>11</li>
       <li>22</li>
       <li>33</li>
     </ul>
-  `;
+  `
 }
 
-export function componentA() {
-  let ul = document.createElement('ul');
+export function componentA () {
+  let ul = document.createElement('ul')
 
   ul.innerHTML = `
     <li>11</li>
     <li>22</li>
     <li>33</li>
-  `;
-
-  return ul;
+  `
+  return ul
 }
