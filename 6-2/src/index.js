60cf8c313396d995b8b1e30774f595f2513e8526
Yang Ke <saint_yk@163.com>
2018-05-27 16:16:15 +0800

    Custom configure react-hot-loader

--- 6-2/src/index.js
+++ 6-2/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './css/base.less';
+import { AppContainer } from 'react-hot-loader';
 
 fetch("/api/comments/show?id=4199740256395164&page=1")
   .then(res => {
@@ -11,5 +12,20 @@ fetch("/api/comments/show?id=4199740256395164&page=1")
       .then(data => { console.log(data); });
   });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>
+  , document.getElementById('root'));
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+render(App);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
