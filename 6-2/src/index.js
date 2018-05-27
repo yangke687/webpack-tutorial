317ecb214529848c1cb21bd7ab73430b3555a56c
Yang Ke <saint_yk@163.com>
2018-05-27 15:30:45 +0800

    Custom configure proxy settings

--- 6-2/src/index.js
+++ 6-2/src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+fetch("/api/comments/show?id=4199740256395164&page=1")
+  .then(res => {
+    res.json()
+      .then(data => { console.log(data); });
+  });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
